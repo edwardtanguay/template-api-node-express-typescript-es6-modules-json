f04d8465c5dc8c5ad662917087a04388ed7e0204
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-21 13:27:15 +0100

    changed time

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export const getPort = () => {
 export const apiStatus = (): IApiStatus => {
 	return {
 		status: "live",
-		whenLiveAgainDateTime: "2024-01-15 11:00:00"
+		whenLiveAgainDateTime: "2024-01-21 14:00:00"
 	}
 }
